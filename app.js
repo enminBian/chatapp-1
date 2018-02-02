e5aa4be075ccca6a8d6f77a11c9cf84e30d41777
Xu <s_xu9@tss-hr420-dm31.fc.ca>
2018-02-02 11:31:55 -0500

    this is the chatapp

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const express = require('express'); //include this, just like a php include!
+const express = require('express');//include this, just like a php onclude
 const app = express();
+const io = require('socket.io')();
 
-app.get('/', (req, res)=> {
-  res.sendFile(__dirname + '/index.html');
+// serve up static files
+app.use(express.static('public'));
+
+
+// add routes
+app.use(require('./routes/index'));
+app.use(require('./routes/contact'));
+app.use(require('./routes/portfolio'));
+
+
+const server = app.listen(3000, () => {
+  console.log('listening on the port 3000');
+
 });
 
-app.get('/contact', (req, res)=> {
-  res.sendFile(__dirname + '/contact.html');
-});
+io.attach(server);
 
-app.get('/portfolio', (req, res)=> {
-  res.sendFile(__dirname + '/portfolio.html');
-});
+io.on('connection', socket =>{ //function(socket) {...}
+  console.log('a user has connected!');
+  io.emit('chat message', {for : 'everyone', message : `${socket.id} is here`});
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+  socket.on('disconnect', ()=> {
+    console.log('a user disconnected');
+
+    io.emit('disconnect message', `${socket.id} has left the building!` );
+  })
 });
---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+(() =>{
+  const socket = io();
+
+  let messageList = document.querySelector('ul'),
+      chatForm = document.querySelector('form'),
+      chatMessage = chatForm.querySelector('.message');
+
+
+  function appendMessage(msg) {
+    //debugger;
+    let newMsg = `<li>${msg.message}</li>`;
+    messageList.innerHTML += newMsg;
+  }
+
+  function appendDiscMessage(msg) {
+    //debugger;
+    let newMsg = `<li>${msg}</li>`;
+    messageList.innerHTML += newMsg;
+  }
+
+  function handleSendMessage(msg) {
+    e.preventDefault(); //block default behaviour (page refresh)
+  }
+
+
+
+
+
+
+  chatForm.addEventListener('submit', handleSendMessage, false);
+  socket.addEventListener('chat message', appendMessage, false);
+  socket.addEventListener('disconnect message', appendDiscMessage, false);
+})();
